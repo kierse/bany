a700630adb9651373b67aa565d0b110640d9f601
Kieran Elliott <kierse@pissiphany.com>
2019-02-10 15:58:41 -0800

    creating domain module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,28 @@
+// Based on:
+// https://github.com/gradle/kotlin-dsl/blob/master/samples/multi-kotlin-project/build.gradle.kts
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.21"
+    kotlin("jvm") version "1.3.21" apply false
 }
 
-group = "com.pissiphany.bany"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "com.pissiphany.bany"
+    version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+    repositories {
+        mavenCentral()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
 }
 
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-}
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+//dependencies {
+//    subprojects.forEach {
+//        archives(it)
+//    }
+//}
+

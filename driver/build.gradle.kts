f182cce762a1766b1d67e986f1800322d7386b4b
Kieran Elliott <kierse@pissiphany.com>
2019-02-11 23:53:11 -0800

    added junit5 dependency to each module

--- driver/build.gradle.kts
+++ driver/build.gradle.kts
@@ -4,4 +4,12 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.4.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.0")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }

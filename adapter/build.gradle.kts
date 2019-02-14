1a8d6c61c0057820cebb8726d02f533ea4aa909a
Kieran Elliott <kierse@pissiphany.com>
2019-02-14 13:05:15 -0800

    added moshi support to adapter

--- adapter/build.gradle.kts
+++ adapter/build.gradle.kts
@@ -1,11 +1,17 @@
 plugins {
-    kotlin("jvm")
+    kotlin("jvm") //version "1.3.21"
+    kotlin("kapt") //version "1.3.21"
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     implementation(project(":domain"))
+
+    implementation("com.squareup.retrofit2:retrofit:2.5.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.5.0")
+    implementation("com.squareup.moshi:moshi:1.8.0")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.8.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.4.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.0")
--- driver/build.gradle.kts
+++ driver/build.gradle.kts
@@ -8,8 +8,6 @@ dependencies {
     implementation(project(":adapter"))
     implementation(project(":domain"))
 
-    implementation("com.squareup.retrofit2:retrofit:2.5.0")
-
     testImplementation("org.junit.jupiter:junit-jupiter:5.4.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.0")

10f86329e84065dbd8831251536e909451810b82
Kieran Elliott <kierse@pissiphany.com>
2019-02-14 22:57:10 -0800

    updated BanyConfig and test

--- adapter/src/main/kotlin/com/pissiphany/bany/adapter/config/BanyConfig.kt
+++ adapter/src/main/kotlin/com/pissiphany/bany/adapter/config/BanyConfig.kt
@@ -2,13 +2,11 @@ package com.pissiphany.bany.adapter.config
 
 import com.squareup.moshi.Json
 
-class BanyConfig(@field:Json(name = "ynab_api_token") val ynabApiToken: String, val plugins: Plugins)
+class BanyConfig(@field:Json(name = "ynab_api_token") val ynabApiToken: String, val plugins: List<Plugin>)
 
-class Plugins(val simplii: List<Simplii>)
+class Plugin(val type: String, val username: String, val password: String, val connections: List<Connection>)
 
-class Simplii(val username: String, val password: String, val links: List<Link>)
-
-class Link(
+class Connection(
     val name: String,
     @field:Json(name = "ynab_budget_id") val ynabBudgetId: String,
     @field:Json(name = "ynab_account_id") val ynabAccountId: String,
--- adapter/src/test/kotlin/com/pissiphany/bany/adapter/config/BanyConfigTest.kt
+++ adapter/src/test/kotlin/com/pissiphany/bany/adapter/config/BanyConfigTest.kt
@@ -6,9 +6,29 @@ import com.squareup.moshi.Moshi
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import java.io.File
 
 internal class BanyConfigTest {
+    private val json = """
+        {
+            "ynab_api_token": "token",
+            "plugins": [
+                {
+                    "type": "type",
+                    "username": "username",
+                    "password": "password",
+                    "connections": [
+                        {
+                            "name": "name",
+                            "third_party_account_id": "third_party_account_id",
+                            "ynab_budget_id": "ynab_budget_id",
+                            "ynab_account_id": "ynab_account_id"
+                        }
+                    ]
+                }
+            ]
+        }
+    """
+
     companion object {
         private lateinit var moshi: Moshi
 
@@ -25,10 +45,16 @@ internal class BanyConfigTest {
     @Test
     fun config() {
         val adapter = moshi.adapter(BanyConfig::class.java)
-        val json = File(System.getProperty("user.home") + "/.bany.config").readText()
 
         val config = adapter.fromJson(json) ?: fail("Unable to load config file!")
 
-        config.ynabApiToken
+        assertEquals(config.ynabApiToken, "token")
+        assertEquals(config.plugins[0].type, "type")
+        assertEquals(config.plugins[0].username, "username")
+        assertEquals(config.plugins[0].password, "password")
+        assertEquals(config.plugins[0].connections[0].name, "name")
+        assertEquals(config.plugins[0].connections[0].thirdPartyAccountId, "third_party_account_id")
+        assertEquals(config.plugins[0].connections[0].ynabAccountId, "ynab_account_id")
+        assertEquals(config.plugins[0].connections[0].ynabBudgetId, "ynab_budget_id")
     }
 }

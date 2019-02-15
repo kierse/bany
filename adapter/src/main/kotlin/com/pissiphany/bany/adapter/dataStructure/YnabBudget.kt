a657b88f6dca389d44f7362dbff3eca21b8921cd
Kieran Elliott <kierse@pissiphany.com>
2019-02-14 22:16:53 -0800

    fixed moshi annotation

--- adapter/src/main/kotlin/com/pissiphany/bany/adapter/dataStructure/YnabBudget.kt
+++ adapter/src/main/kotlin/com/pissiphany/bany/adapter/dataStructure/YnabBudget.kt
@@ -1,7 +1,8 @@
 package com.pissiphany.bany.adapter.dataStructure
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import java.time.LocalDateTime
 
 @JsonClass(generateAdapter = true)
-data class YnabBudget(val id: String, val name: String, val last_modified_on: LocalDateTime)
+data class YnabBudget(val id: String, val name: String, @field:Json(name = "last_modified_on") val lastModifiedOn: LocalDateTime)

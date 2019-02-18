be6a1632e307229e155dfd12056dce44e515b662
Kieran Elliott <kierse@pissiphany.com>
2019-02-17 20:32:19 -0800

    added basic file backed config repo

---
+++ adapter/src/main/kotlin/com/pissiphany/bany/adapter/gateway/FileBasedConfigurationRepository.kt
@@ -0,0 +1,17 @@
+package com.pissiphany.bany.adapter.gateway
+
+import com.pissiphany.bany.adapter.config.BanyConfig
+import com.pissiphany.bany.domain.dataStructure.BudgetAccountIds
+import com.pissiphany.bany.domain.gateway.ConfigurationRepository
+
+class FileBasedConfigurationRepository(private val config: BanyConfig) : ConfigurationRepository {
+    // TODO replace with a mapper
+    override fun getBudgetAccountIds(): List<BudgetAccountIds> {
+        val budgetAccountIds = mutableListOf<BudgetAccountIds>()
+        for (plugin in config.plugins) {
+            for(connection in plugin.connections) {
+                budgetAccountIds.add(BudgetAccountIds(connection.ynabBudgetId, connection.ynabAccountId))
+            }
+        }
+    }
+}

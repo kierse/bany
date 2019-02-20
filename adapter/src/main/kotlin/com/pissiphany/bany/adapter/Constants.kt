9edd07089d68c903e2ee262d7206b16f394ace72
Kieran Elliott <kierse@pissiphany.com>
2019-02-20 11:53:11 -0800

    make constant private

--- adapter/src/main/kotlin/com/pissiphany/bany/adapter/Constants.kt
+++ adapter/src/main/kotlin/com/pissiphany/bany/adapter/Constants.kt
@@ -5,7 +5,7 @@ import java.io.File
 const val BASE_URL = "https://api.youneedabudget.com/"
 
 object Constants {
-    val CONFIG_DIR = File(System.getProperty("user.home"), ".bany")
+    private val CONFIG_DIR = File(System.getProperty("user.home"), ".bany")
     val CONFIG_FILE = File(CONFIG_DIR, "bany.config")
     val LAST_KNOWLEDGE_OF_SERVER_FILE = File(CONFIG_DIR, "last_knowledge_of_server.properties")
 }

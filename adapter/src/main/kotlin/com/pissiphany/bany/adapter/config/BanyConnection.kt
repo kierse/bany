d48c2af2dc9034a2407723390b2b8371b0a6fc3e
Kieran Elliott <kierse@pissiphany.com>
2019-03-06 12:07:21 -0800

    updated config data structures to implement bany-config interfaces

--- adapter/src/main/kotlin/com/pissiphany/bany/adapter/config/BanyConnection.kt
+++ adapter/src/main/kotlin/com/pissiphany/bany/adapter/config/BanyConnection.kt
@@ -1,12 +1,13 @@
 package com.pissiphany.bany.adapter.config
 
+import com.pissiphany.bany.adapter.plugin.BanyPlugin
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 class BanyConnection(
     val name: String,
-    @Json(name = "ynab_budget_id") val ynabBudgetId: String,
-    @Json(name = "ynab_account_id") val ynabAccountId: String,
-    @Json(name = "third_party_account_id") val thirdPartyAccountId: String
-)
+    @Json(name = "ynab_budget_id") override val ynabBudgetId: String,
+    @Json(name = "ynab_account_id") override val ynabAccountId: String,
+    @Json(name = "third_party_account_id") override val thirdPartyAccountId: String
+) : BanyPlugin.Connection
--- adapter/src/main/kotlin/com/pissiphany/bany/adapter/config/BanyPluginConfiguration.kt
+++ adapter/src/main/kotlin/com/pissiphany/bany/adapter/config/BanyPluginConfiguration.kt
@@ -1,8 +1,12 @@
 package com.pissiphany.bany.adapter.config
 
+import com.pissiphany.bany.adapter.plugin.BanyPlugin
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 class BanyPluginConfiguration(
-    val username: String, val password: String, val connections: List<BanyConnection>, val enabled: Boolean = true
-)
+    override val username: String,
+    override val password: String,
+    override val connections: List<BanyConnection>,
+    override val enabled: Boolean = true
+) : BanyPlugin.Configuration

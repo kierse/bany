678ebd4cde6bdcd95f9f8a99121215a86c16eb7e
Kieran Elliott <kierse@pissiphany.com>
2019-03-06 12:07:57 -0800

    mapper test

---
+++ adapter/src/test/kotlin/com/pissiphany/bany/adapter/mapper/BanyPluginTransactionMapperTest.kt
@@ -0,0 +1,18 @@
+package com.pissiphany.bany.adapter.mapper
+
+import com.pissiphany.bany.adapter.plugin.BanyPluginTransaction
+import com.pissiphany.bany.domain.dataStructure.Transaction
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import java.time.LocalDate
+
+internal class BanyPluginTransactionMapperTest {
+    @Test
+    fun toTransaction() {
+        val transaction = Transaction("id", LocalDate.now(), 10L)
+        val pluginTransaction = BanyPluginTransaction(transaction.id, transaction.date, transaction.amount)
+
+        assertEquals(transaction, BanyPluginTransactionMapper().toTransaction(pluginTransaction))
+    }
+}

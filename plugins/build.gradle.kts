0352965341a29129e307dfce4ef95fe148334040
Kieran Elliott <kierse@pissiphany.com>
2019-03-13 16:53:49 -0700

    removed disabled.txt and enabled.txt as pf4j isn't using them

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -34,22 +34,7 @@ subprojects
         }
     }
 
-tasks.register<Copy>("pluginConfiguration") {
-    val pluginsDir: String by rootProject.extra
-
-    // copy disabled.txt into pluginsDir
-    from("${projectDir.absolutePath}/disabled.txt")
-    into(pluginsDir)
-
-    // copy enabled.txt into pluginsDir
-    from("${projectDir.absolutePath}/enabled.txt")
-    into(pluginsDir)
-}
-
 tasks.named("build") {
-    // make "build" task depend on custom task named "pluginConfiguration"
-    dependsOn(project.tasks["pluginConfiguration"])
-
     // make "build" task depend on all sub-project custom tasks named "assemblePlugin"
     subprojects.forEach { dependsOn("${it.name}:assemblePlugin") }
 }

7b20d05fb72b8d90f58e29f94e1cc5878cc450c8
Kieran Elliott <kierse@pissiphany.com>
2019-03-15 18:02:29 -0700

    added Simplii environment support

--- plugins/cibc/src/main/kotlin/com/pissiphany/bany/plugin/cibc/CibcTransactionServiceFactory.kt
+++ plugins/cibc/src/main/kotlin/com/pissiphany/bany/plugin/cibc/CibcTransactionServiceFactory.kt
@@ -3,26 +3,39 @@ package com.pissiphany.bany.plugin.cibc
 import com.pissiphany.bany.plugin.BanyPlugin
 import com.pissiphany.bany.plugin.BanyPluginFactory
 import com.pissiphany.bany.plugin.cibc.environment.CibcEnvironment
+import com.pissiphany.bany.plugin.cibc.environment.SimpliiEnvironment
 import com.pissiphany.bany.plugin.cibc.mapper.CibcTransactionMapper
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import org.pf4j.Extension
 import java.lang.IllegalArgumentException
+import java.net.CookieManager
+import java.net.CookiePolicy
 
-private const val CIBC = "cibc"
+internal const val CIBC = "cibc"
+internal const val SIMPLII = "simplii"
 
 @Extension
 class CibcTransactionServiceFactory : BanyPluginFactory {
-    override val pluginNames = setOf(CIBC)
+    override val pluginNames = setOf(CIBC, SIMPLII)
+
+    private val moshi = Moshi.Builder().build()
 
     override fun createPlugin(pluginName: String, credentials: BanyPlugin.Credentials): BanyPlugin {
         val env = when (pluginName) {
             CIBC -> CibcEnvironment()
+            SIMPLII -> SimpliiEnvironment()
             else -> throw IllegalArgumentException("unknown/unsupported plugin '$pluginName'")
         }
 
+        val jar = QuotePreservingCookieJar(CookieManager(null, CookiePolicy.ACCEPT_ALL))
+        val client = OkHttpClient
+            .Builder()
+            .cookieJar(jar)
+            .build()
+
         return CibcTransactionService(
-            credentials, env, Moshi.Builder().build(), OkHttpClient(), CibcTransactionMapper()
+            credentials, env, moshi, client, CibcTransactionMapper()
         )
     }
 }
---
+++ plugins/cibc/src/main/kotlin/com/pissiphany/bany/plugin/cibc/environment/SimpliiEnvironment.kt
@@ -0,0 +1,13 @@
+package com.pissiphany.bany.plugin.cibc.environment
+
+class SimpliiEnvironment : Environment {
+    override val baseUrl = "https://online.simplii.com"
+    override val staticUrl = "$baseUrl/static/e677a8bd0c8192970126258a37cefd2"
+    override val authUrl = "$baseUrl/ebm-anp/api/v1/json/sessions"
+    override val accountsUrl = "$baseUrl/ebm-ai/api/v1/json/accounts"
+    override val transactionsUrl = "$baseUrl/ebm-ai/api/v1/json/transactions"
+    override val refererUrl = "$baseUrl/ebm-resources/public/client/web/index.html"
+
+    override val host = "online.simplii.com"
+    override val brand = "simplii"
+}

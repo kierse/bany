0371d8ee2c2a0c5b73dba4cba3d1a48ddf8a9f29
Kieran Elliott <kierse@pissiphany.com>
2019-03-16 18:18:45 -0700

    added annotation so moshi will generate adapters at compile time

--- plugins/cibc/src/main/kotlin/com/pissiphany/bany/plugin/cibc/dataStructure/CibcAccountsWrapper.kt
+++ plugins/cibc/src/main/kotlin/com/pissiphany/bany/plugin/cibc/dataStructure/CibcAccountsWrapper.kt
@@ -1,5 +1,9 @@
 package com.pissiphany.bany.plugin.cibc.dataStructure
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class CibcAccountsWrapper(val accounts: List<CibcAccount>) {
+    @JsonClass(generateAdapter = true)
     data class CibcAccount(val id: String, val number: String, val balance: String, val currency: String)
 }
--- plugins/cibc/src/main/kotlin/com/pissiphany/bany/plugin/cibc/dataStructure/CibcTransactionWrapper.kt
+++ plugins/cibc/src/main/kotlin/com/pissiphany/bany/plugin/cibc/dataStructure/CibcTransactionWrapper.kt
@@ -1,14 +1,18 @@
 package com.pissiphany.bany.plugin.cibc.dataStructure
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class CibcTransactionWrapper(val transactions: List<CibcTransaction> = emptyList()) {
+    @JsonClass(generateAdapter = true)
     data class CibcTransaction(
         val id: String,
         val accountId: String,
         val date: String,
         val descriptionLine1: String,
         val transactionDescription: String,
-        val credit: String,
-        val debit: String
+        val credit: String? = null,
+        val debit: String? = null
 //        val transactionType: CibcTransactionType
     )
 //    enum class CibcTransactionType {

632a699002650c2d1e20f06890cbb97194fee9e5
Kieran Elliott <kierse@pissiphany.com>
2022-02-11 21:03:45 -0500

    deleting YnabBudgetAccountsGateway and YnabBudgetAccountsGatewayImpl

--- domain/src/main/kotlin/com/pissiphany/bany/domain/gateway/YnabBudgetAccountsGateway.kt
+++
@@ -1,8 +0,0 @@
-package com.pissiphany.bany.domain.gateway
-
-import com.pissiphany.bany.domain.dataStructure.Account
-import com.pissiphany.bany.domain.dataStructure.BudgetAccountIds
-
-interface YnabBudgetAccountsGateway {
-    fun getAccount(budgetAccountIds: BudgetAccountIds): Account?
-}

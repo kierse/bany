b3783d9acb18665db75114954ae7fc473b849acf
Kieran Elliott <kierse@pissiphany.com>
2022-06-14 15:16:47 -0600

    Added coroutines to domain test

--- domain/src/test/kotlin/com/pissiphany/bany/domain/useCase/step/GetAccountDetailsTest.kt
+++ domain/src/test/kotlin/com/pissiphany/bany/domain/useCase/step/GetAccountDetailsTest.kt
@@ -3,15 +3,18 @@ package com.pissiphany.bany.domain.useCase.step
 import com.pissiphany.bany.domain.dataStructure.*
 import com.pissiphany.bany.domain.gateway.YnabAccountDetailsGateway
 import com.pissiphany.bany.domain.repository.YnabLastKnowledgeOfServerRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import java.time.OffsetDateTime
 
+@OptIn(ExperimentalCoroutinesApi::class)
 internal class GetAccountDetailsTest {
 
     @Test
-    fun getTransaction() {
+    fun getTransaction() = runTest {
         val budgetAccountIds = BudgetAccountIds(name = "name", budgetId = "budgetId", accountId = "accountId")
         val account = Account("id", "name", 0, false, Account.Type.CASH)
         val transactions = listOf(
@@ -31,11 +34,11 @@ internal class GetAccountDetailsTest {
         var lastKnowledge = 1
             private set
 
-        override fun getLastKnowledgeOfServer(budgetAccountIds: BudgetAccountIds): Int {
+        override suspend fun getLastKnowledgeOfServer(budgetAccountIds: BudgetAccountIds): Int {
             return if (budgetAccountIds.accountId == "accountId") lastKnowledge else 0
         }
 
-        override fun saveLastKnowledgeOfServer(budgetAccountIds: BudgetAccountIds, lastKnowledgeOfServer: Int) {
+        override suspend fun saveLastKnowledgeOfServer(budgetAccountIds: BudgetAccountIds, lastKnowledgeOfServer: Int) {
             lastKnowledge = lastKnowledgeOfServer
         }
     }
@@ -44,9 +47,9 @@ internal class GetAccountDetailsTest {
         private val account: Account,
         private val transactions: List<AccountTransaction>
     ) : YnabAccountDetailsGateway {
-        override fun getAccount(budgetAccountIds: BudgetAccountIds) = account
+        override suspend fun getAccount(budgetAccountIds: BudgetAccountIds) = account
 
-        override fun getUpdatedTransactions(
+        override suspend fun getUpdatedTransactions(
             budgetAccountIds: BudgetAccountIds,
             lastKnowledgeOfServer: Int
         ): UpdatedTransactions {

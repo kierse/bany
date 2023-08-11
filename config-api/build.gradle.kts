638f69b2d0ba2fd71ead43aef7770470e2ea6715
Kieran Elliott <kierse@pissiphany.com>
2023-08-11 13:26:23 -0400

    Added kapt to generate moshi adapters

--- config-api/build.gradle.kts
+++ config-api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 group = "com.pissiphany.bany"
@@ -8,4 +9,5 @@ version = "1.0-SNAPSHOT"
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(libs.square.moshi)
+    kapt(libs.square.moshi.codegen)
 }
